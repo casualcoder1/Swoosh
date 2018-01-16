688080d619f1919665e8e4418f2e4d5c690b55e2
Michael Chow <mchow8@gmail.com>
2018-01-16 23:44:58 +1100

    lesson 3

--- app/src/main/java/com/michaelchow/swoosh/WelcomeActivity.kt
+++ app/src/main/java/com/michaelchow/swoosh/WelcomeActivity.kt
@@ -1,12 +1,21 @@
 package com.michaelchow.swoosh
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcome.*
 
 class WelcomeActivity : AppCompatActivity() {
 
+    // First function that is called when the activity is created
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcome)
+
+        getStartedBtn.setOnClickListener {
+            // Intent is an object that lets you interact with other applications as well as your own.
+            val leagueIntent = Intent(this, LeagueActivity::class.java) // This is an explicit intent.
+            startActivity(leagueIntent)
+        }
     }
 }
